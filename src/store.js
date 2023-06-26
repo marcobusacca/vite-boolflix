8815d92ffaef00b2a404ae126796238e9e8bc64a
marcobusacca <marcobusacca01@gmail.com>
2023-06-26 15:01:20 +0200

    [MILESTONE 0] store.js

--- src/store.js
+++ src/store.js
@@ -4,4 +4,11 @@ import { reactive } from "vue";
 
 // ESPORTO LA CONSTANTE STORE CONTENENTE TUTTI GLI ELEMENTI ACCESSIBILI AD OGNI COMPONENTE VUE
 export const store = reactive({
+    ApiKey: '31cb166d3bf37ee613d74902c213a580',
+    MoviesApiUri: 'https://api.themoviedb.org/3/search/movie',
+    SeriesApiUri: 'https://api.themoviedb.org/3/search/tv',
+    ImageUri: 'https://image.tmdb.org/t/p/w342',
+    MoviesList: [],
+    SeriesList: [],
+    userSearch: '',
 });
